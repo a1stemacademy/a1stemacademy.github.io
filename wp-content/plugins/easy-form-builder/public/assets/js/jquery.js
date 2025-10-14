d10c19645b004471a18fc9b48cda9a28e436d58f
sudo-veeru <pveerendra@hotmail.com>
2025-10-13 23:08:42 -0400

    New Version

---
+++ wp-content/plugins/easy-form-builder/public/assets/js/jquery.js
@@ -0,0 +1,11 @@
+
+
+    document.getElementById("choices-multiple-remove-button").addEventListener("change", (e) => {
+        e.preventDefault();      
+        const el = document.getElementById(e.target.id);
+        for (let i = 0; i < el.children.length; i++) {
+       
+        }
+      });
+
+ 
